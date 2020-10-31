53045fcb584731fe44c573984f12d1f94212cfc2
PABLO ALEXIS CRISTOBAL GALLEGOS CELIS <pablo.gallegos@uc.cl>
2020-10-30 21:00:34 -0300

    cambios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,13 +45,12 @@
     <!-- Chart JS -->
     <script src="{{ asset('js/plugins/chartjs.min.js')}}"></script>
     <!--  Notifications Plugin    -->
-    <script src="{{ asset('paper') }}/js/plugins/bootstrap-notify.js"></script>
+    <script src="{{ asset('js/plugins/bootstrap-notify.js')}}"></script>
     <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="{{ asset('paper') }}/js/paper-dashboard.min.js?v=2.0.0" type="text/javascript"></script>
+    <script src="{{ asset('js/paper-dashboard.min.js?v=2.0.0')}}" type="text/javascript"></script>
     <!-- Paper Dashboard DEMO methods, don't include it in your project! -->
    
-    <!-- Sharrre libray -->
-    <script src="../assets/demo/jquery.sharrre.js"></script>
+    
     
     @stack('scripts')
 
